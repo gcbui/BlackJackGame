73cc2e100f93f663b87830eb0f0643f23ab443cf
gcbui <you@example.com>
2022-05-16 21:42:30 -0700

    Prepared steps for doc. Created variables and funcs.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # BlackJackGame
+Steps
+Deck with 52 cards
+Shuffle cards
+Distribute 2 cards to player then 2 cards to dealer
+Show 1 card for dealer
+Player can hit, stand, or double down (times n)
+Check to see if player busted (times n)
+Check to see if dealers cards add up to 17 or more
+    If 17 or more and less than 22, then compare cards
+    If not, then hit card for dealer
+
+
+Objects (data structures)
+    Player
+        Cards = list of type card
+        Name
+    Card
+        Number
+        Suit
+
---
+++ scripts/main.py
@@ -0,0 +1,37 @@
+class card:
+    number = 0
+    suit = "undef"
+class player:
+    name = "undeclared"
+    deck = []
+
+global_deck = []
+dealer_deck = []
+
+def generate_cards():
+    pass
+
+def distribute_cards():       #give player 2 cards and dealer 2 cards
+    pass
+
+def show_dealer_card():
+    pass
+
+def hit(cards):
+    pass
+
+def stand():
+    pass
+
+def double_down():
+    pass
+
+def check_bust(cards):
+    pass
+
+def player_logic():          #takes input of player hit, stand, or doubledown
+    pass
+
+def dealer_logic():
+    pass
+
